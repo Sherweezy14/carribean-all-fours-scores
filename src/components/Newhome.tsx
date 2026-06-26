8c995938736a1793bfebf9f633f3b17060e80545
Sherwyn Cooper <newenglandballas@gmail.com>
2026-06-26 02:08:08 -0400

    finished tournament stats component

--- src/components/Newhome.tsx
+++ src/components/Newhome.tsx
@@ -1,18 +1,17 @@
 import {
     Radio,
-    Users,
-    Clock
   
   } from "lucide-react";
 import ScoresCard from "./Scorescard";
 import Rankings from "./Rankings";
+import TournamentStats from "./Tournamentstats";
 
 export default function NewHome(){
 
 
     return(
         <section className="grid grid-cols-1 lg:grid-cols-3 gap-2 px-5 py-5 grid-flow-row-dense ">
-            <div className="col-span-1 md:col-span-2 pl-2 flex flex-col gap-2 justify-between">
+            <div className="col-span-1 md:col-span-2 pl-2 flex flex-col gap-2 ">
                 <div className="flex justify-between pb-5">
                     <div className="flex md:flex-col">
                         <div className="flex gap-2 pt-2">
@@ -33,6 +32,7 @@ export default function NewHome(){
             
             <div className="col-span-1">
                 <Rankings />
+                <TournamentStats />
             </div>
 
         </section> )
---
+++ src/components/Tournamentstats.tsx
@@ -0,0 +1,29 @@
+import {Target,Gamepad,Spade} from "lucide-react"
+export default function TournamentStats(){
+
+
+
+    return(
+        <section className="px-2 font-display py-3 border-2 border-gray-100 mt-3 rounded-md flex flex-col">
+            <div className="  text-blue-950 text-2xl tracking-wide">Tournment STATS
+            </div>
+            <div className="flex justify-between">
+                <div className="flex flex-col items-center">
+                    <Target size={48} className="text-red-700"/>
+                    <p className="text-2xl">Bullseyes</p>
+                    <p className="text-xl">142</p>
+                </div>
+                <div className="flex flex-col items-center">
+                    <Gamepad size={48} className="text-yellow-400" />
+                    <p className="text-2xl">Hangjacks</p>
+                    <p className="text-xl">97</p>
+                </div>
+                <div className="flex flex-col items-center">
+                    <Spade size={48} className="text-blue-950" />
+                    <p className="text-2xl">Games played</p>
+                    <p className="text-xl">34</p>
+                </div>
+            </div>
+        </section>
+    )
+}
