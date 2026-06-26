a316a144ea2915a05576b9cc86166aebb00fbfba
Sherwyn Cooper <newenglandballas@gmail.com>
2026-06-26 18:28:58 -0400

    added key tok quik navs loop and body and header for rnakings table

--- src/components/Quicknav.tsx
+++ src/components/Quicknav.tsx
@@ -1,35 +1,32 @@
-import {
-    Radio,
-    Calendar,
-    Trophy,
-    Users,
-    Layers,
-  } from "lucide-react";
+import { Radio, Calendar, Trophy, Users, Layers } from "lucide-react";
 
-export default function QuickNav(){
-    const links = [
-    {name:"LIVE", icon:Radio ,route:"/"},
-    {name:"SCHEDULE", icon:Calendar ,route:"/"},
-    {name:"RANKINGS", icon:Trophy ,route:"/"},
-    {name:"TEAMS", icon:Users ,route:"/"},
-    {name:"DIVISIONS", icon:Layers ,route:"/"},]
+export default function QuickNav() {
+  const links = [
+    { name: "LIVE", icon: Radio, route: "/" },
+    { name: "SCHEDULE", icon: Calendar, route: "/" },
+    { name: "RANKINGS", icon: Trophy, route: "/" },
+    { name: "TEAMS", icon: Users, route: "/" },
+    { name: "DIVISIONS", icon: Layers, route: "/" },
+  ];
 
-
-
-
-
-    return(<section className="grid grid-cols-5 px-5 gap-2 md:gap-5 mt-4 lg:hidden">
-        {links.map((link)=>{
-            return(<div className="group
-            hover:shadow-lg
-            hover:-translate-y-1
-            transition-all
-            duration-200
-            cursor-pointer  hover:text-white hover:bg-red-700 flex flex-col bg-[#f8fafc] items-center justify-center h-16 rounded-md shadow-sm border-gray-100 border-2 md:h-20">
-            <link.icon className=" mb-1"/>
+  return (
+    <section className="mt-4 grid grid-cols-5 gap-2 px-5 md:gap-5 lg:hidden">
+      {links.map((link) => {
+        return (
+          <div
+            key={link.name}
+            className="group
+            flex
+            h-16
+            cursor-pointer
+            flex-col
+            items-center  justify-center rounded-md border-2 border-gray-100 bg-[#f8fafc] shadow-sm transition-all duration-200 hover:-translate-y-1 hover:bg-red-700 hover:text-white hover:shadow-lg md:h-20"
+          >
+            <link.icon className=" mb-1" />
             <p className="text-xs">{link.name}</p>
-            </div>)
-        })}
-        
-    </section>)
+          </div>
+        );
+      })}
+    </section>
+  );
 }
--- src/components/Rankings.tsx
+++ src/components/Rankings.tsx
@@ -1,44 +1,46 @@
-export default function Rankings(){
+export default function Rankings() {
+  return (
+    <section>
+      <div className="rounded-md border-2 border-gray-100 px-2 py-3">
+        <div className="flex flex-col">
+          <div className="flex items-center lg:flex-col lg:items-start">
+            <span className="mr-2 font-display text-2xl text-blue-950">
+              Top Rankings
+            </span>
+            <span className="text-red-500"> Division A</span>
+          </div>
+          <div className="overflow-hidden rounded-lg border-2 border-gray-100 text-center text-sm lg:text-xl">
+            <table className="w-full">
+              <thead>
+                <tr className="bg-slate-100 font-display tracking-wide">
+                  <td>#</td>
+                  <td>team</td>
+                  <td>W</td>
+                  <td>PTS</td>
+                  <td>Bullseyes</td>
+                  <td>Hangjacks</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Four Play</td>
+                  <td>4</td>
+                  <td>12</td>
+                  <td>20</td>
+                  <td>8</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
-
-
-
-
-    return(
-        <section>
-            <div className="border-2 border-gray-100 rounded-md px-2 py-3">
-                <div className="flex flex-col">
-                    <div className="flex items-center lg:flex-col lg:items-start">
-                        <span className="font-display text-blue-950 text-2xl mr-2">Top Rankings</span>
-                        <span className="text-red-500" > Division A</span>
-                    </div>
-                    <div className="border-2 overflow-hidden border-gray-100 text-sm lg:text-xl rounded-lg text-center">
-                    <table className="w-full">
-                        <tr className="font-display tracking-wide bg-slate-100">
-                            <td>#</td>
-                            <td>team</td>
-                            <td>W</td>
-                            <td>PTS</td>
-                            <td>Bullseyes</td>
-                            <td>Hangjacks</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Four Play</td>
-                            <td>4</td>
-                            <td>12</td>
-                            <td>20</td>
-                            <td>8</td>
-                        </tr>
-                    </table>
-                    </div>
-                    
-                    <div className="bg-red-700 rounded-md shadow-md cursor-pointer mt-2">
-                        <p className="text-white font-display text-xl text-center">View full Rankings</p>
-                    </div>
-                    
-                </div>
-            </div>
-        </section>
-    )
+          <div className="mt-2 cursor-pointer rounded-md bg-red-700 shadow-md">
+            <p className="text-center font-display text-xl text-white">
+              View full Rankings
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
