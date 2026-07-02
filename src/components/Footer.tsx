4f59513c3030b8c51a5822870d1b68f5bf27809c
Sherwyn Cooper <newenglandballas@gmail.com>
2026-07-02 14:20:55 -0400

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,29 @@
-import flag from "../assets/tt-flag-01-XL.png"
-import logo from "../assets/logo_nav.png"
-export default function Footer(){
-    return(
-        <footer style={{
-            backgroundImage: `url(${flag})`,
-          }} className="bg-cover bg-center bg-no-repeat w-full flex items-center h-20 px-5 py-2">
-                <div className="h-24"><img className="h-24" src={logo} alt="" /></div>
-                <div className="flex flex-col text-white">
-                    <p className="font-display text-2xl tracking-wider"> Unity sports club </p>
-                    <p className="text-sm"> Building Community Through Sports & Culture </p>
-                    <p className="text-yellow-500 font-mono "> One Community One Unity</p>
-                </div>
-        </footer>
-    )
+import flag from "../assets/tt-flag-01-XL.png";
+import logo from "../assets/logo_nav.png";
+
+export default function Footer() {
+  return (
+    <footer className="relative flex h-20 items-center justify-center overflow-hidden bg-[#041533] px-5 py-2 lg:justify-start">
+      {/* Background image only on lg+ */}
+      <div
+        className="absolute inset-0 hidden bg-cover bg-center bg-no-repeat lg:block"
+        style={{ backgroundImage: `url(${flag})` }}
+      />
+
+      {/* Content */}
+      <div className="relative z-10 flex items-center">
+        <img className="h-24" src={logo} alt="Unity Sports Club Logo" />
+
+        <div className="flex flex-col text-white">
+          <p className="font-display text-2xl tracking-wider">
+            Unity Sports Club
+          </p>
+
+          <p className="text-sm">Building Community Through Sports & Culture</p>
+
+          <p className="font-mono text-yellow-500">One Community • One Unity</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
